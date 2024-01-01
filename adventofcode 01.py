90860221f597eea12edf6e31c2a9ed61947e1191
st3fan3ich <stefan.eich44@gmail.com>
2024-01-01 23:46:07 +0100

    fixed errors, works now

--- adventofcode 01.py
+++ adventofcode 01.py
@@ -12,43 +12,60 @@ coord = []                                              # empty array, to be fil
 
 regex = '[\d]+[.,\d]+|[\d]*[.][\d]+|[\d]+'                  # define what you are looking for with re.search
 
-for i in range(10):
+
+
+
+
+
+for i in range(1000):
     s = file.readline()
     #print(s)                         # string you are filtering
     filter = []
 
     if re.search(regex, s) is not None:                         # filter funktion
-        for catch in re.finditer(p, s):
+        for catch in re.finditer(regex, s):
             filter.append(catch[0])
             #print(catch[0])                                         # catch is a match object, for testing
             #print(filter)                                           # print filtered array, for testing
 
-            a = filter[0]                                           # get first number from filtered array
-            b = filter[-1]                                          # get last number from filtered array
-            if int(a) > 999:                                        # check for exceptions first integer > 999
-                a1 = int(a)//1000
-            elif int(a) > 99:                                       # check for exceptions first integer > 99
-                a1 = int(a)//100                                  # take the first digit, discard the rest
-            elif int(a) > 9:                                        # check for exceptions first integer > 9
-                a1 = int(a)//10                                   # take the first digit, discard the rest
-            else:
-                a1 = int(a)
-
-            if int(b) > 99:                                         # check for exceptions last integer > 99
-                b1 = int(b)%10                                    # take the last digit, discard the rest
-            elif int(b) > 9:                                          # check for exceptions last integer > 9
-                b1 = int(b)%10                                    # take the last digit, discard the rest
-            else:
-                b1 = int(b)
+        a = filter[0]                                           # get first number from filtered array
+        b = filter[-1]                                          # get last number from filtered array
+        #print(filter)
+        if int(a) > 999:                                        # check for exceptions first integer > 999
+            a1 = int(a)//1000
+            #print("a>999", a1)
+        elif int(a) > 99:                                       # check for exceptions first integer > 99
+            a1 = int(a)//100 
+            #print("a>99", a1)                                 # take the first digit, discard the rest
+        elif int(a) > 9:                                        # check for exceptions first integer > 9
+            a1 = int(a)//10
+            #print("a>9", a1)                                   # take the first digit, discard the rest
+        else:
+            a1 = int(a)
+            #print("a<9", a1)
+                                    
+        if int(b) > 9:                                          # check for exceptions last integer > 9
+            b1 = int(b)%10                                    # take the last digit, discard the rest
+            #print("b>9", b1)
+        else:
+            b1 = int(b)
+            #print("b<9", b1)
       
 #print(a1)
 #print(b1)
-            c = str(a1) + str(b1)                                   # form the number a + b = ab (1 + 1 = 11)
+    c = str(a1) + str(b1)                                   # form the number a + b = ab (1 + 1 = 11)
 #print(c)
-            coord.append(int(c))
-            print(coord, i)
+    coord.append(int(c))
+            #print(coord, i)
 print(coord)
 
+"""
+with open("input_file01.txt", "r") as file:
+    for line in file:
+        if re.search(regex, s) is not None:                         # filter funktion
+        for catch in re.finditer(p, s):
+            filter.append(catch[0])
+"""
 #print(27%10) 
 #print(93//10)
 #test = [2, 4, 3, 9, 1]
