32ee6d2cdd90890b10114c342735db72800582b2
st3fan3ich <stefan.eich44@gmail.com>
2024-01-02 18:21:41 +0100

    finish task, works

--- adventofcode01.py
+++ adventofcode01.py
@@ -18,24 +18,43 @@ solution = 0
 
 # def convert_first():
 #     if matches[0] == "one":
-#             matches[0] = 1
+#             matches[0] = "1"
 #         elif matches[0] == "two":
-#             matches[0] = 2
+#             matches[0] = "2"
 #         elif matches[0] == "three":
-#             matches[0] = 3
+#             matches[0] = "3"
 #         elif matches[0] == "four":
-#             matches[0] = 4
+#             matches[0] = "4"
 #         elif matches[0] == "five":
-#             matches[0] = 5
+#             matches[0] = "5"
 #         elif matches[0] == "six":
-#             matches[0] = 6
+#             matches[0] = "6"
 #         elif matches[0] == "seven":
-#             matches[0] = 7
+#             matches[0] = "7"
 #         elif matches[0] == "eight":
-#             matches[0] = 8
+#             matches[0] = "8"
 #         elif matches[0] == "9":
-#             matches[0] = 9
-#         break
+#             matches[0] = "9"
+
+# def convert_last():
+#     if matches[-1] == "one":
+#             matches[-1] = "1"
+#         elif matches[-1] == "two":
+#             matches[-1] = "2"
+#         elif matches[-1] == "three":
+#             matches[-1] = "3"
+#         elif matches[-1] == "four":
+#             matches[-1] = "4"
+#         elif matches[-1] == "five":
+#             matches[-1] = "5"
+#         elif matches[-1] == "six":
+#             matches[-1] = "6"
+#         elif matches[-1] == "seven":
+#             matches[-1] = "7"
+#         elif matches[-1] == "eight":
+#             matches[-1] = "8"
+#         elif matches[-1] == "nine":
+#             matches[-1] = "9"
 
 
 regex = re.compile(r"(one|two|three|four|five|six|seven|eight|nine|\d)")
@@ -43,22 +62,42 @@ with open("input_file01.txt", "r") as file:
     for line in file:
         matches = [m[0] for m in regex.finditer(line)]
         if matches[0] == "one":
-            matches[0] = 1
+            matches[0] = "1"
         elif matches[0] == "two":
-            matches[0] = 2
+            matches[0] = "2"
         elif matches[0] == "three":
-            matches[0] = 3
+            matches[0] = "3"
         elif matches[0] == "four":
-            matches[0] = 4
+            matches[0] = "4"
         elif matches[0] == "five":
-            matches[0] = 5
+            matches[0] = "5"
         elif matches[0] == "six":
-            matches[0] = 6
+            matches[0] = "6"
         elif matches[0] == "seven":
-            matches[0] = 7
+            matches[0] = "7"
         elif matches[0] == "eight":
-            matches[0] = 8
-        elif matches[0] == "9":
-            matches[0] = 9
-        break
-print(matches[0])
+            matches[0] = "8"
+        elif matches[0] == "nine":
+            matches[0] = "9"
+        if matches[-1] == "one":
+            matches[-1] = "1"
+        elif matches[-1] == "two":
+            matches[-1] = "2"
+        elif matches[-1] == "three":
+            matches[-1] = "3"
+        elif matches[-1] == "four":
+            matches[-1] = "4"
+        elif matches[-1] == "five":
+            matches[-1] = "5"
+        elif matches[-1] == "six":
+            matches[-1] = "6"
+        elif matches[-1] == "seven":
+            matches[-1] = "7"
+        elif matches[-1] == "eight":
+            matches[-1] = "8"
+        elif matches[-1] == "nine":
+            matches[-1] = "9"
+        number = matches[0] + matches[-1]
+        solution += int(number)
+# print(matches[0])
+print(solution)
